f9424743c3751755ea054036d1a12bcb435850b9
Cristiancastano-95 <62962848+Cristiancastano-95@users.noreply.github.com>
2020-12-14 19:27:25 -0500

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,3 @@
-$(function() {
-    $("html, body, *").mousewheel(function(event, delta) {
-        this.scrollLeft -= (delta * 80);
-        this.scrollRight -= (delta * 80);
-        event.preventDefault();
-    });
-});
+function menu() {
+    document.getElementById("container-menu").style.right="30%";
+};
